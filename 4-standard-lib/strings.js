8f3e3a337b7381283f45f35064c7b6398efa00b3
Maina Beth <mainabethy@gmail.com>
2019-11-29 00:35:07 +0300

    third test

--- 4-standard-lib/strings.js
+++ 4-standard-lib/strings.js
@@ -48,7 +48,9 @@ describe("strings", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
-
+function letterAt(string){
+    return text.charAt(string);
+}
             ////////////////////////////////////////////////////////////////////
 
             expect(letterAt(0)).to.equal("T");
