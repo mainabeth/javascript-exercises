91fc48ec55e11196de097e1ccdb32aa9e3903394
Maina Beth <mainabethy@gmail.com>
2019-11-28 21:05:13 +0300

    fourteenth test

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -317,7 +317,8 @@ describe("functions", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you implement a function 'myFunction' that returns a function?
             // ✏️ ADD CODE HERE ✏️
-
+            function myFunction(){};
+                return function () {};
             ////////////////////////////////////////////////////////////////////
 
             const result = myFunction();
